af657d1bdad091ca7cadd9004766155760f24bb3
Mason <mason.menser@wsu.edu>
2023-10-16 11:24:22 -0700

    add build

---
+++ MA3/build/CMakeFiles/MA3.exec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MA3.exec.dir/TwitterData.cpp.o"
+  "CMakeFiles/MA3.exec.dir/TwitterData.cpp.o.d"
+  "CMakeFiles/MA3.exec.dir/main.cpp.o"
+  "CMakeFiles/MA3.exec.dir/main.cpp.o.d"
+  "MA3.exec"
+  "MA3.exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MA3.exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MA3/build/CMakeFiles/MA3.exec.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MA3.exec.
+# This may be replaced when dependencies are built.
